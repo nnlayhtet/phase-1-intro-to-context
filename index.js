136125d42e4009d9faaec6677c76abed00c75f0b
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-13 10:55:41 -0400

    commit tree 8347a3aa5e9cf4de94660de3ae73e660cda2f728

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+//console.log(createEmployeeRecord(['nyi','htet','student']))
+
+function createEmployeeRecords(arrOfArrays){
+    return arrOfArrays.map(arr=>createEmployeeRecord(arr))
+}
